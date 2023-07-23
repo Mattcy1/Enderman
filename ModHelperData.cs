2d050c725b77b89496df997fdc4a621a74375626
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-07-24 00:00:21 +0200

    In game menu no more update to the mod

---
+++ ModHelperData.cs
@@ -0,0 +1,16 @@
+﻿namespace Enderman
+{
+    public static class ModHelperData
+    {
+        public const string WorksOnVersion = "37.0";
+        public const string Version = "1.0";
+        public const string Name = "Enderman";
+        public const string Description =
+            "Adds in a new custom tower the enderman Extra credit to minecraft not more update only middle path just to much issue so this is the last version working before the issue";
+        
+
+        public const string RepoOwner = "Mattcy1";
+        public const string RepoName = "Enderman";
+        public const string Icon = "Enderman-Icon.png";
+    }
+}
